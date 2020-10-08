28feda124be8880172a41e361d323819e0a3fbfe
Karthik Moulya <karthik@pop-os.localdomain>
2020-10-08 18:58:23 +0530

    added logout to the application

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
-const config = require('./config/key');
+const config = require("./config/key");
 
 const app = express();
 
-const { User } = require('./models/user');
+const { User } = require("./models/user");
+const { auth } = require("./middleware/auth");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -18,21 +19,62 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
+    useFindAndModify: false,
   })
-  .then(() => console.log('DB connected'))
-  .catch(err => console.error(err));
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.error(err));
 
-app.get('/', (req, res) => {
-  res.json({ 'hello ~': 'world ~~~~~' });
+app.get("/api/user/auth", auth, (req, res) => {
+  res.status(200).json({
+    _id: req._id,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+  });
 });
 
-app.post('/api/users/register', (req, res) => {
+app.post("/api/users/register", (req, res) => {
   const user = new User(req.body);
 
-  user.save((err, userData) => {
+  user.save((err, doc) => {
     if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
   });
-  return res.status(200).json({ success: true });
+});
+
+app.post("/api/user/login", (req, res) => {
+  //find the email
+  User.findOne({ email: req.body.email }, function (err, user) {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "Auth failed, email not found",
+      });
+    }
+    //comparePassword
+
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        return res.json({ loginSuccess: false, message: "wrong password" });
+      }
+    });
+
+    //generateToken
+    user.generateToken((err, user) => {
+      if (err) return res.status(400).send(err);
+      res.cookie("x_auth", user.token)
+        .status(200).json({ loginSuccess: true });
+    });
+  });
+});
+
+app.get("/api/user/logout", auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, doc) => {
+    if (err) return res.status(400).json({ success: false, err });
+    return res.status(200).send({ success: true });
+  });
 });
 
 app.listen(5000, () => console.log(`sever runnig is at port: 5000 `));
